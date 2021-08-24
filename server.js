8abb1dd73e59137799f3fe67e41ad031296ca179
thomas <thomas@vps-b98080b1.vps.ovh.net>
2021-08-24 20:25:42 +0000

    Https server listener

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ let path = require('path');
 let eng = require("./engine/engine");
 let mm = require("./minmax/algo");
 
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+
+var privateKey  = fs.readFileSync('/etc/letsencrypt/live/vps.thomaslepercq.fr/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('/etc/letsencrypt/live/vps.thomaslepercq.fr/cert.pem', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
 const app = express();
 
 //=========================
@@ -49,7 +56,8 @@ app.get('/ai/:board/:turn', function (req, res) {
 	})
 });
 
-app.listen(3333);
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(3333);
 
 // let {test} = require("./engine/test");
 // test();
